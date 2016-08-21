2739bae1965a5b0ec1302b88d4e1a1145ebef754
taoria <445625470@qq.com>
2016-08-22 01:57:52 +0800

    A naodong way of generating air-islands

--- java/cn/paindar/test/worldgen/WorldGeneratorFloatingIsland.java
+++ java/cn/paindar/test/worldgen/WorldGeneratorFloatingIsland.java
@@ -13,6 +13,65 @@ import java.util.Random;
 /**
  * Created by Paindar on 2016/8/19.
  */
+class IslandGenerator{
+	private int wid;
+	private int hei;
+	private int len;
+	private int IslandType;
+	private int gay[][][];
+	public static final int ISLAND_TYPE_ROUND=1;
+	public static final int ISLAND_TYPE_SQURE=2;
+	public static final int ISLAND_TYPE_TRIAN=3;
+	
+	public IslandGenerator(int len,int hei,int type){
+		this.wid=this.len=len;
+		this.hei=hei;
+		gay  = new int[hei][len][len];
+		if(type==IslandGenerator.ISLAND_TYPE_ROUND){
+			int decay[][][] = new int[hei][len][len];
+			int cX= len/2;
+			int cZ= len/2;
+			int cY = (hei*2)/3;
+			for(int y=hei-1;y>=0;y--){
+				for(int x=0;x<len;x++){
+					for(int z=0;z<len;z++){
+						if(y==hei-1){
+							decay[y][x][z]=len/2-1-(int)(Math.random()+0.2);
+						}else{
+							decay[y][x][z]=decay[y+1][x][z]-(int)(Math.random()+0.7);
+						}
+					}
+				}
+			}
+			for(int x=0;x<len;x++)
+				for(int z=0;z<len;z++)
+					decay[hei-1][x][z]=decay[hei-1][x][z]-=(int)(Math.random()*3+0.7);
+			for(int y=hei-1;y>=0;y--){
+				for(int x=0;x<len;x++){
+					for(int z=0;z<len;z++){
+						double cmp  = Math.round(Math.sqrt((x-cX)*(x-cX)+(z-cZ)*(z-cZ)))-decay[y][x][z];
+						if(cmp<=0){
+							if(y>=hei-3){
+								gay[y][x][z]=1;
+							}else{
+								if(cmp<=-1)
+								gay[y][x][z]=2;
+								else gay[y][x][z]=1;
+							}
+							
+						}else{
+							gay[y][x][z]=0;
+						}
+					}
+				}
+			}
+		}
+			
+	}
+	public int [][][] GetGeneratedIsland(){
+		return gay;
+	}
+}
 public class WorldGeneratorFloatingIsland implements IWorldGenerator
 {
 
@@ -22,36 +81,51 @@ public class WorldGeneratorFloatingIsland implements IWorldGenerator
         return low+(high-low)* MathHelper.sin(rate*1.57f);
     }
 
-    private static float getGray(int gray[][][],int x,int y,int z, int strX,int strY,int strZ,int endX,int endY,int endZ,int maxG)
-    {
-        if(gray[y][x][z]!=maxG)
-            return gray[y][x][z];
-        if(z!=strZ && z!=endZ)
-            gray[y][x][z]=(int)(lerpf(getGray(gray,x,y,strZ,strX,strY,strZ,endX,endY,endZ,maxG),getGray(gray,x,y,endZ,strX,strY,strZ,endX,endY,endZ,maxG),(float)(endZ-z)/(endZ-strZ)));
-        else if(y!=strY && y!=endY)
-            gray[y][x][z]=(int)(lerpf(getGray(gray,x,strY,z,strX,strY,strZ,endX,endY,endZ,maxG),getGray(gray,x,endY,z,strX,strY,strZ,endX,endY,endZ,maxG),(float)(endY-y)/(endY-strY)));
-        else if(x!=strX&& x!=endX)
-            gray[y][x][z]=(int)(lerpf(getGray(gray,strX,y,z,strX,strY,strZ,endX,endY,endZ,maxG),getGray(gray,endX,y,z,strX,strY,strZ,endX,endY,endZ,maxG),(float)(endX-x)/(endX-strX)));
-        else
-            gray[y][x][z]=maxG-1;
-        return gray[y][x][z];
-    }
-    private static void fill(int gray[][][], int strX,int strY,int strZ,int endX,int endY,int endZ,int maxG)
-    {
-        for(int x=strX;x<=endX;x++)
-        {
-            for(int y=strY;y<=endY;y++)
-            {
-                for (int z=strZ;z<=endZ;z++)
-                {
-                    if(gray[y][x][z]==maxG)
-                    {
-                        getGray(gray,x,y,z,strX,strY,strZ,endX,endY,endZ,maxG);
-                    }
-                }
-            }
-        }
-    }
+//    private static float getGray(int gray[][][],int x,int y,int z, int strX,int strY,int strZ,int endX,int endY,int endZ,int maxG)
+//    {
+//
+//        if(gray[y][x][z]==maxG)
+//        {
+//            if(x<=strX || x>=endX || y<=strY || y>=endY || z<=strZ || z>=endZ)
+//            {
+//                if(z!=strZ && z!=endZ)
+//                    gray[y][x][z]=(int)(maxG*MathHelper.sin(1.57f*(getGray(gray,x,y,endZ,strX,strY,strZ,endX,endY,endZ,maxG)*(endZ-z)+getGray(gray,x,y,strZ,strX,strY,strZ,endX,endY,endZ,maxG)*(z-strZ))/((endZ-strZ)*maxG)));
+//                else if(y!=strY && y!=endY)
+//                    gray[y][x][z]=(int)(maxG*MathHelper.sin(1.57f*(getGray(gray,x,endY,z,strX,strY,strZ,endX,endY,endZ,maxG)*(endY-y)+getGray(gray,x,strY,z,strX,strY,strZ,endX,endY,endZ,maxG)*(y-strY))/((endY-strY)*maxG)));
+//                else if(x!=strX&& x!=endX)
+//                    gray[y][x][z]=(int)(maxG*MathHelper.sin(1.57f*(getGray(gray,endX,y,z,strX,strY,strZ,endX,endY,endZ,maxG)*(endX-x)+getGray(gray,strX,y,z,strX,strY,strZ,endX,endY,endZ,maxG)*(x-strX))/((endX-strX)*maxG)));
+//                else
+//                    gray[y][x][z]=maxG-1;
+//            }
+//            else
+//            {
+//                 gray[y][x][z]=(int)(
+//                     getGray(gray,x,y+1,z,strX,strY,strZ,endX,endY,endZ,maxG)
+//                    +getGray(gray,x,y-1,z,strX,strY,strZ,endX,endY,endZ,maxG)
+//                    +getGray(gray,x+1,y,z,strX,strY,strZ,endX,endY,endZ,maxG)
+//                    +getGray(gray,x-1,y,z,strX,strY,strZ,endX,endY,endZ,maxG)
+//                    +getGray(gray,x,y,z+1,strX,strY,strZ,endX,endY,endZ,maxG)
+//                    +getGray(gray,x,y,z-1,strX,strY,strZ,endX,endY,endZ,maxG))/6;
+//            }
+//        }
+//        return gray[y][x][z];
+//    }
+//    private static void fill(int gray[][][], int strX,int strY,int strZ,int endX,int endY,int endZ,int maxG)
+//    {
+//        for(int x=strX;x<=endX;x++)
+//        {
+//            for(int y=strY;y<=endY;y++)
+//            {
+//                for (int z=strZ;z<=endZ;z++)
+//                {
+//                    if(gray[y][x][z]==maxG)
+//                    {
+//                        getGray(gray,x,y,z,strX,strY,strZ,endX,endY,endZ,maxG);
+//                    }
+//                }
+//            }
+//        }
+//    }
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
@@ -59,7 +133,7 @@ public class WorldGeneratorFloatingIsland implements IWorldGenerator
         //
         int x=chunkX*16+8,y=255,z=chunkZ*16+8;
         Chunk chunk=world.getChunkFromChunkCoords(chunkX,chunkZ);
-        if(chunk.getBiomeGenForWorldCoords(0,0,world.getWorldChunkManager())!= BiomeGenBase.coldTaigaHills || MathHelper.getRandomDoubleInRange(random,0,1f)<=0.2f)
+        if( MathHelper.getRandomDoubleInRange(random,0,1f)>=0.02f)
         {
             return;
         }
@@ -73,114 +147,29 @@ public class WorldGeneratorFloatingIsland implements IWorldGenerator
         }
         if(y<=50||y>150)return;
         y+=40;
-        /*
-
-
-        for(int i=-8;i<=8;i++)
-        {
-            for(int j=-8;j<=8;j++)
-            {
-                //double factor=5*Math.pow(2.73,-((i*i)+(j*j))/24);
-                int factor=MathHelper.abs_int(i)+MathHelper.abs_int(j);
-                int height=random.nextInt(MathHelper.ceiling_double_int(5*MathHelper.cos(3.14f*factor / 32)));
-                //int height=MathHelper.ceiling_double_int(factor);
-                if(height==0&&factor<=8)height=1;
-                for(int k=0;k<height;k++)
-                {
-                    if(!world.isRemote)
-                        world.setBlock(x+i,y-k,z+j,Blocks.stone);
-                    //√1/(16-x)
-                }
-
-            }
-        }*/
-        int MAXSIZE=16,MaxGray=255;
-        int grayLvl[][][]=new int[MAXSIZE][MAXSIZE][MAXSIZE];
-        for(int ay=0;ay<MAXSIZE;ay++)
-        {
-            for (int ax = 0; ax < MAXSIZE; ax ++)
-            {
-                for (int az = 0; az < MAXSIZE; az ++)
-                {
-                    grayLvl[ay][ax][az]=MaxGray;
-                }
-            }
-        }
-
-        for(int i=2;i<MAXSIZE;i+=2)
-        {
-            for(int j=2;j<MAXSIZE;j+=2)
-            {
-                grayLvl[8][i][j]=0+(i-8)*(i-8)*2+(j-8)*(j-8)*2;
-                grayLvl[12][i][j]=random.nextInt(MaxGray-grayLvl[8][i][j])+grayLvl[8][i][j];
-                grayLvl[4][i][j]=random.nextInt(MaxGray-grayLvl[8][i][j])+grayLvl[8][i][j];
-                grayLvl[0][i][j]=random.nextInt(grayLvl[4][i][j]+1);
-            }
-        }
-        for(int i=2;i<MAXSIZE;i+=2)
-        {
-            for(int j=2;j<MAXSIZE;j+=2)
-            {
-                for(int k=4;k<MAXSIZE-2;k+=2)
-                {
-                    if(grayLvl[k][i][j]==MaxGray)
-                    {
-                        grayLvl[k][i][j]=(int)(lerpf(grayLvl[k+2][i][j],grayLvl[k-2][i][j],0.5f));
-                    }
-                }
-            }
-        }
-
-        for(int ay=0;ay<16;ay+=2)
-        {
-            for(int ax=0;ax<16;ax+=2)
-            {
-                for(int az=0;az<16;az+=2)
-                {
-                    int maxX=(ax+2>=16)?15:ax+2,
-                            maxY=(ay+4>=16)?15:ay+4,
-                            maxZ=(az+2>=16)?15:az+2;
-                    fill(grayLvl,ax,ay,az,maxX,maxY,maxZ,MaxGray);
-                }
-            }
-        }
+        
+        int MAXSIZE=16,MaxGray=1000;
+        int grayLvl[][][]=new IslandGenerator(16,8,1).GetGeneratedIsland();
+        exc=true;
         if(exc)
         {
-            for (int ay = 0; ay < 16; ay++) {
-                for (int ax = 0; ax < 16; ax++) {
-                    for (int az =0; az < 16; az++) {
-                        if (grayLvl[ay][ax][az] <= MaxGray/4) {
-                            world.setBlock(x + ax - 8, y + ay - 8, z + az - 8, Blocks.stone);
-                        }
-                        else if(grayLvl[ay][ax][az] <= MaxGray/2)
+            for (int ay = 0; ay < grayLvl.length; ay++) {
+                for (int ax = 0; ax < grayLvl[0].length; ax++) {
+                    for (int az =0; az <  grayLvl[0][0].length; az++) {
+                        if (grayLvl[ay][ax][az] == 1) {
                             world.setBlock(x + ax - 8, y + ay - 8, z + az - 8, Blocks.dirt);
-                        else
-                            world.setBlockToAir(x + ax - 8, y + ay - 8, z + az - 8);
-
-
-
+                          //  System.out.println("test");
+                        }
+                        if (grayLvl[ay][ax][az] == 2) {
+                            world.setBlock(x + ax - 8, y + ay - 8, z + az - 8, Blocks.stone);
+                          //  System.out.println("test");
+                        }
                     }
+                    
                 }
             }
-            //exc=false;
+            System.out.println("test");
         }
-        else
-        {
-            int offset=0;
-            while(world.getBlock(x,y-offset,z)!=Blocks.dirt)
-                offset++;
-            for (int ax = -7; ax < 7; ax++) {
-                for (int az = -7; az < 7; az++) {
-                    for(int ay=0;ay>=(ax*ax+az*az)/9.6-5;ay--)
-                    {
-                        world.setBlock(x + ax , y + ay , z + az , world.getBlock(x+ax,y+ay-offset,z+az));
-                    }
-                }
-            }
-
-            exc=true;
-        }
-
         System.out.println("("+x+","+y+","+z+")");
     }
 }
